186e1899cf3cd19dde54753c5b0991d58a7ecb4a
Nícolas Costa <nicolas.nico@hotmail.com>
2023-09-10 07:30:07 -0300

    feat: add grpc server

--- cmd/grpc/main.go
+++ cmd/grpc/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"google.golang.org/grpc"
+	"grpc/internal/pb"
+	"grpc/internal/services"
+	"net"
+)
+
 func main() {
 
+	server := grpc.NewServer()
+	pb.RegisterContactServiceServer(server, services.NewContactService())
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		panic(err)
+	}
+
+	if err = server.Serve(lis); err != nil {
+		panic(err)
+	}
 }
